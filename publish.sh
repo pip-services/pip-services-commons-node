ba466ad33e0e43c99bad958563337f964211f2ed
Sergey Seroukhov <seroukhov@gmail.com>
2017-09-09 16:59:08 +0300

    Added publish script

--- publish.sh
+++ publish.sh
@@ -6,8 +6,9 @@ VERSION=`cat ./VERSION`
 # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 set -e
 
-#npm login
-#git tag "v${VERSION}"
+npm login
+
+git tag "v${VERSION}"
 git push --tags
 
 npm publish
