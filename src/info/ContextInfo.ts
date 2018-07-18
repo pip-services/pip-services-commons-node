44f16d9c3bd171d770e696ebe46dc947ffad781a
Mark Makarychev <markmak77@gmail.com>
2018-07-18 14:56:01 +0300

    Pre-release TypeDoc for info.

--- src/info/ContextInfo.ts
+++ src/info/ContextInfo.ts
@@ -5,6 +5,10 @@ import { StringValueMap } from '../data/StringValueMap';
 import { ConfigParams } from '../config/ConfigParams';
 import { IReconfigurable } from '../config/IReconfigurable';
 
+/**
+ * A simple class that defines the context of execution. Used for various logging functions, where information 
+ * about the source that is being logging must be known (what's the process's name, what is it, what does it do). 
+ */
 export class ContextInfo implements IReconfigurable {	
 	private _name: string = "unknown";
 	private _description: string = null;
@@ -12,38 +16,80 @@ export class ContextInfo implements IReconfigurable {
 	private _startTime: Date = new Date();
 	private _properties: StringValueMap = new StringValueMap();
 
+	/**
+	 * @param name  		(optional) name of the context of execution. Defaults to "unknown" if omitted.
+	 * @param description 	(optional) description of the context of execution. Defaults to null if omitted.
+	 */
 	public constructor(name?: string, description?: string) {
 		this._name = name || "unknown";
 		this._description = description || null;
 	}
 
+	/**
+	 * Sets component's configuration parameters and switches the component
+	 * to the 'Configured' state. 
+	 * 
+	 * @param config 	the ConfigParams to configure this object with.
+	 * 
+	 * @see ConfigParams
+	 * @see IConfigurable
+	 */
 	public configure(config: ConfigParams): void {
 		this.name = config.getAsStringWithDefault("name", this.name);
 		this.description = config.getAsStringWithDefault("description", this.description);		
 		this.properties = config.getSection("properties");
 	}
 	
+	/** Gets the name of the context of execution. */
 	public get name(): string { return this._name; }
+	/** 
+	 * Sets the name of the context of execution. 
+	 * @param value		name to set. Defaults to "unknown" if null.
+	 */
 	public set name(value: string) { this._name = value || "unknown"; }
 	
+	/** Gets the description of the context of execution. */
 	public get description(): string { return this._description; }
+	/** Set the description of the context of execution. */
 	public set description(value: string) { this._description = value; }
 	
+	/** Gets the id of the context of execution. */
 	public get contextId(): string { return this._contextId; }
+	/** Sets the id of the context of execution. */
 	public set contextId(value: string) { this._contextId = value; }
 	
+	/** Gets the time at which the execution was started. */
 	public get startTime(): Date { return this._startTime; }
+	/** Sets the time at which the execution was started. */
 	public set startTime(value: Date) { this._startTime = value || new Date(); }
-	
+
+	/** @returns the amount of time that has passed since the execution started. */
 	public get uptime(): number {
 		return new Date().getTime() - this._startTime.getTime();
 	}
 
+	/** Gets the properties of the context of execution. */
 	public get properties(): any { return this._properties; }
+	/** 
+	 * Sets the properties of the context of execution. 
+	 * 
+	 * @param properties 	values that will be converted to a StringValueMap and saved
+	 * 						to this ContextInfo's properties.
+	 * 
+	 * @see StringValueMap
+	*/
 	public set properties(properties: any) {
 		this._properties = StringValueMap.fromValue(properties);
 	}
 	
+	/**
+	 * Static method that creates and configures a ContextInfo object using the ConfigParams 
+	 * passed in 'config'.
+	 * 
+	 * @param config 	ConfigParams to use when configuring the new ContextInfo object.
+	 * 
+	 * @see ConfigParams
+	 */
 	public static fromConfig(config: ConfigParams): ContextInfo {
 		let result = new ContextInfo();
 		result.configure(config);
--- src/info/DefaultInfoFactory.ts
+++ src/info/DefaultInfoFactory.ts
@@ -3,12 +3,24 @@ import { Descriptor } from '../refer/Descriptor';
 
 import { ContextInfo } from './ContextInfo';
 
+/**
+ * Contains static read-only descriptors for ContextInfo and ContainerInfo (as well as a default info descriptor). 
+ * There are two different ContainerInfoDescriptors for backward compatibility purposes.
+ * 
+ * @see Factory
+ */
 export class DefaultInfoFactory extends Factory {
 	public static readonly Descriptor: Descriptor = new Descriptor("pip-services", "factory", "info", "default", "1.0");
 	public static readonly ContextInfoDescriptor: Descriptor = new Descriptor("pip-services", "context-info", "default", "*", "1.0");
 	public static readonly ContainerInfoDescriptor: Descriptor = new Descriptor("pip-services", "container-info", "default", "*", "1.0");
 	public static readonly ContainerInfoDescriptor2: Descriptor = new Descriptor("pip-services-container", "container-info", "default", "*", "1.0");
 	
+    /**
+	 * Registers the ContextInfoDescriptor, ContainerInfoDescriptor, and ContainerInfoDescriptor2 (backward compatibility) 
+	 * descriptors as types in this factory.
+	 * 
+	 * @see Factory#Factory
+	 */
 	public constructor() {
 		super();
 		this.registerAsType(DefaultInfoFactory.ContextInfoDescriptor, ContextInfo);
